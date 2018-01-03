cac4aefced2a2bb472a8f060f2a20abe4219357c
bersling <bersling@gmail.com>
2018-01-03 07:38:02 +0100

    10000 dragable boxes, no optimizations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
-
+import {BoxComponent} from './box.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BoxComponent
   ],
   imports: [
     BrowserModule
